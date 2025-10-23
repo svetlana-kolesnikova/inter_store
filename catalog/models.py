f45657207158e2b6731027618472c4e5d0af239b
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-10-23 23:39:15 +0300

    add 0007_alter_product_image_alt

--- catalog/models.py
+++ catalog/models.py
@@ -18,16 +18,15 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200, verbose_name="Наименование", help_text="Введите название продукта")
+    name = models.CharField(max_length=200, verbose_name="Наименование")
 
     description = models.TextField(verbose_name="Описание")
 
     image = models.ImageField(
-        upload_to="products/", blank=True, null=True, verbose_name="Изображение", help_text="Загрузите фото продукта"
-    )
+        upload_to="products/", blank=True, null=True, verbose_name="Изображение")
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена", help_text="Введите цену товара")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
 
