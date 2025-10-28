299e3aa5aa85cf2deb005a4c91a391e8d52724ae
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-10-28 16:08:13 +0300

    fixes catalog files

--- catalog/models.py
+++ catalog/models.py
@@ -23,7 +23,8 @@ class Product(models.Model):
     description = models.TextField(verbose_name="Описание")
 
     image = models.ImageField(
-        upload_to="products/", blank=True, null=True, verbose_name="Изображение")
+        upload_to="products/", blank=True, null=True, default="img/no_image.png", verbose_name="Изображение"
+    )
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
