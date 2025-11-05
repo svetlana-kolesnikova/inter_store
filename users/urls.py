9f2f6d1f6def201a1de0bdb005664dfbc2be0aff
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-11-05 22:43:48 +0300

    add files for users

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import RegisterView
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='catalog:home'), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+]
