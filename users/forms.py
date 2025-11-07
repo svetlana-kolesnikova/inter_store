07be1bc5584efb5a47e2741f38e662462af10c22
svetlana-kolesnikova <sv.ps1286@gmail.com>
2025-11-07 23:34:40 +0300

    fixes for users

--- users/forms.py
+++ users/forms.py
@@ -1,20 +1,31 @@
 from django import forms
-from  django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    phone_number = forms.CharField(max_length=15, required=False, help_text='Необязательное поле. Введите ваш номер телефона.')
+    phone_number = forms.CharField(
+        max_length=15, required=False, help_text="Необязательное поле. Введите ваш номер телефона."
+    )
     username = forms.CharField(max_length=50, required=True)
     usable_password = None
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ('email', 'username', 'first_name', 'last_name', 'phone_number', 'password1', 'password2', 'country',)
+        fields = (
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "password1",
+            "password2",
+            "country",
+        )
 
     def clean_phone_number(self):
-        phone_number = self.cleaned_data.get('phone_number')
+        phone_number = self.cleaned_data.get("phone_number")
         if phone_number and not phone_number.isdigit():
-            raise forms.ValidationError('Phone number must contain only digits.')
+            raise forms.ValidationError("Phone number must contain only digits.")
         return phone_number
